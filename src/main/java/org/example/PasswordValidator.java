9d748114880c7adb5714d79cd8296622855da497
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-10-14 14:17:26 +0200

    pom org updated; basic design added: empty methods and respective empty tests

---
+++ src/main/java/org/example/PasswordValidator.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+public final class PasswordValidator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    // Methods
+
+    public static boolean hasMinLength(String password) {
+    }
+
+    public static boolean containsDigit(String password) {
+    }
+
+    public static boolean containsUpperAndLower(String password) {
+    }
+
+    public static boolean isCommonPassword(String password) {
+    }
+
+    // Bonus methods
+    public static boolean containsSpecialCharacter(String password) {
+    }
+
+    public static boolean isValid(String password) {
+    }
+}
---
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class PasswordValidatorTest {
+
+    @Test
+    void hasMinLength() {
+    }
+
+    @Test
+    void containsDigit() {
+    }
+
+    @Test
+    void containsUpperAndLower() {
+    }
+
+    @Test
+    void isCommonPassword() {
+    }
+
+    @Test
+    void containsSpecialCharacter() {
+    }
+
+    @Test
+    void isValid() {
+    }
+}
